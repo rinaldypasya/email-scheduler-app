e143d708bd42d8d214f8740667d83b4e3b5aa1e4
Rinaldy Pasya <rinaldypasya@gmail.com>
2024-10-18 00:08:35 +0800

    feat: set email credential configurable (#2)

--- app/email_utils.py
+++ app/email_utils.py
@@ -1,8 +1,11 @@
 import smtplib
 from email.mime.text import MIMEText
+import os
 
 def send_email(recipient, subject, body):
-    sender = "your-email@example.com"
+    sender = os.getenv('EMAIL_SENDER')
+    password = os.getenv('EMAIL_PASSWORD')
+    
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = sender
@@ -10,5 +13,5 @@ def send_email(recipient, subject, body):
     
     with smtplib.SMTP('smtp.your-email-provider.com', 587) as server:
         server.starttls()
-        server.login(sender, "your-password")
+        server.login(sender, password)
         server.sendmail(sender, [recipient], msg.as_string())
